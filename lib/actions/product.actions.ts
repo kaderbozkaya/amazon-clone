3f816fd94fdab9ad0cf7400a0c1816b5be5c2555
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2025-01-23 13:52:04 +0300

    create product details page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,5 +1,7 @@
 "use server"
 
+import { PAGE_SIZE } from "../constants"
+
 import { connectToDatabase } from "../db"
 import Product, { IProduct } from "../db/models/product.model"
 
@@ -60,3 +62,45 @@ const products=await Product.find({
 .limit(limit)
 return JSON.parse(JSON.stringify(products))as IProduct[]
 }
+
+//Get one product by slug
+
+export async function getProductBySlug(slug:string) {
+    await connectToDatabase()
+    const product=await Product.findOne({slug,isPublished:true})
+    if(!product) throw new Error("Product not found")
+  return JSON.parse(JSON.stringify(product)) as IProduct
+}
+
+//aynı kategorideki ilgili ürünleri al
+
+export async function getRelatedProductsByCategory({
+    category,
+    productId,
+    limit=PAGE_SIZE,
+    page=1,
+}: {
+    category:string
+    productId:string
+    limit?:number
+    page:number
+}) {
+    await connectToDatabase()
+    const skipAmount=(Number(page)-1)*limit //sorgunun kaç tane ürünü atlayacağını belirtir.Sayfa 1: Hiçbir ürün atlanmaz (skipAmount = 0).Sayfa 2: İlk limit kadar ürün atlanır. örn page:2 limit=10 ise skipAmount=(2-1)*10=10
+
+    const conditions={
+        isPubliched:true,
+        category,
+        _id:{$ne:productId} //idsi productId olan sonuçlara dahil edilmeyecek.
+    }
+    const products=await Product.find(conditions)
+    .sort({numSales:"desc"}) //ürünleri satış sayısına göre azalan sırada sıralar.
+    .skip(skipAmount)
+    .limit(limit)
+    const productsCount=await Product.countDocuments(conditions) //Koşullara uyan tüm ürünlerin sayısını döndürür. Bu, toplam sayfa sayısını hesaplamak için kullanılır.
+    return{
+        data:JSON.parse(JSON.stringify(products)) as IProduct[],
+        totalPages:Math.ceil(productsCount/limit) //productCount = 45, limit = 10 ise toplam sayfa sayısı 5 olur.
+    }
+}
+
