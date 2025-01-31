72f14438032a624f4509d3c8745a46151e56968e
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2025-01-31 21:57:56 +0300

    create cart page

--- hooks/use-cart-store.ts
+++ hooks/use-cart-store.ts
@@ -1,81 +1,139 @@
-import { calcDeliveryDateAndPrice } from "@/lib/actions/order.actions";
-import { Cart, OrderItem } from "@/types";
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { create } from 'zustand' //zustand store oluşturmak için
+import { persist } from 'zustand/middleware' //depolama alanına kaydetmek için
 
+import { Cart, OrderItem } from '@/types'
+import { calcDeliveryDateAndPrice } from '@/lib/actions/order.actions'
 
-const initialState:Cart={
-    items:[],
-    itemsPrice:0,
-    taxPrice:undefined,
-    shippingPrice:undefined,
-    totalPrice:0,
-    paymentMethod:undefined,
-    deliveryDateIndex:undefined,
+const initialState: Cart = {
+  items: [], //sepetteki ürünler
+  itemsPrice: 0,
+  taxPrice: undefined,
+  shippingPrice: undefined,
+  totalPrice: 0,
+  paymentMethod: undefined,
+  shippingAddress: undefined,
+  deliveryDateIndex: undefined,
 }
-interface CartStore {
+interface CartState {
     cart:Cart
-    addItem:(item:OrderItem,quantity:number)=>Promise<string>
+    addItem: (item: OrderItem, quantity: number) => Promise<string>
+  updateItem: (item: OrderItem, quantity: number) => Promise<void>
+  removeItem: (item: OrderItem) => void
 }
 
 //zustand mağaza tanımlama
 
-const useCartStore=create(
-    //persist sepet durumunun tarayıcıda (ör. localStorage) kalıcı olarak saklanmasını sağlar.
-    persist<CartStore>(
-        (set,get)=>({
-            cart:initialState,
-
-            addItem:async(item:OrderItem,quantity:number)=> {
-                const {items}=get().cart
-                const existItem=items.find(
-                    (x)=>
-                        x.product===item.product &&
-                    x.color===item.color &&
-                    x.size===item.size
-                )
-                 // Eğer ürün zaten sepette varsa, miktarı güncelle
-                if(existItem){
-                    if(existItem.countInStock<quantity+existItem.quantity){
-                        throw new Error('No enough items in stock')
-                    }
-                }else {
-                    // Ürün sepette değilse, miktarın stokta olup olmadığını kontrol et
-                    if(item.countInStock<item.quantity){
-                        throw new Error('Not enough items in stock')
-                    }
-                }
-                const updateCartItems=existItem ?
-                items.map((x)=>
-                x.product===item.product &&
-            x.color===item.color &&
-            x.size===item.size 
-            ?{...existItem,quantity:existItem.quantity+quantity}
-            :x
+const useCartStore = create(
+    persist<CartState>(
+      (set, get) => ({ //set ile statei güncellemek get ile mevcut statei okumak için
+        cart: initialState,
+  
+        addItem: async (item: OrderItem, quantity: number) => {
+          const { items } = get().cart //sepetteki ürünleri al
+          //sepette aynı ürün var mı kontrolu
+          const existItem = items.find(
+            (x) =>
+              x.product === item.product &&
+              x.color === item.color &&
+              x.size === item.size
+          )
+          //eğer ürün sepette varsa stoku kontrol eder yeni ürün ekliyorsa stok yeterlimi diye bakar
+  
+          if (existItem) {
+            if (existItem.countInStock < quantity + existItem.quantity) {
+              throw new Error('Not enough items in stock')
+            }
+          } else {
+            if (item.countInStock < item.quantity) {
+              throw new Error('Not enough items in stock')
+            }
+          }
+  //ürün sepette varsa miktarını arttırır.yoksa yeni ürün ekler
+          const updatedCartItems = existItem
+            ? items.map((x) =>
+                x.product === item.product &&
+                x.color === item.color &&
+                x.size === item.size
+                  ? { ...existItem, quantity: existItem.quantity + quantity }
+                  : x
+              )
+            : [...items, { ...item, quantity }]
+  //teslimat tarihi ve kargo ücretini hesaplar
+          set({
+            cart: {
+              ...get().cart,
+              items: updatedCartItems,
+              ...(await calcDeliveryDateAndPrice({
+                items: updatedCartItems,
+              
+              })),
+            },
+          })
+          //sepete eklenen ürününn client id değerini döndürür
+          const foundItem = updatedCartItems.find(
+            (x) =>
+              x.product === item.product &&
+              x.color === item.color &&
+              x.size === item.size
+          )
+          if (!foundItem) {
+            throw new Error('Item not found in cart')
+          }
+          return foundItem.clientId
+        },
+        //güncellemek istenen ürün sepette var mı kontrol edilir yoksa işlem yapılmaz.
+        updateItem: async (item: OrderItem, quantity: number) => {
+          const { items} = get().cart
+          const exist = items.find(
+            (x) =>
+              x.product === item.product &&
+              x.color === item.color &&
+              x.size === item.size
+          )
+          if (!exist) return
+          //eğer ürün sepette varsa yeni miktarla güncellenir teslimat ve kargo tekrar hesaplanır.
+          const updatedCartItems = items.map((x) =>
+            x.product === item.product &&
+            x.color === item.color &&
+            x.size === item.size
+              ? { ...exist, quantity: quantity }
+              : x
+          )
+          set({
+            cart: {
+              ...get().cart,
+              items: updatedCartItems,
+              ...(await calcDeliveryDateAndPrice({
+                items: updatedCartItems,
+            
+              })),
+            },
+          })
+        },
+        //
+        removeItem: async (item: OrderItem) => {
+            const { items} = get().cart
+        const updatedCartItems = items.filter(
+          (x) =>
+            x.product !== item.product ||
+            x.color !== item.color ||
+            x.size !== item.size
         )
-        : [...items,{...item,quantity}]
-        // Yeni sepeti durumuna ekle ve fiyatları hesapla
         set({
-            cart:{
-                ...get().cart,
-                items:updateCartItems,
-                ...(await calcDeliveryDateAndPrice({
-                    items:updateCartItems,
-                })), //Güncellenmiş sepeti ve fiyat bilgilerini (calcDeliveryDateAndPrice) hesaplatarak durumu yeniler.
-            },
+          cart: {
+            ...get().cart,
+            items: updatedCartItems,
+            ...(await calcDeliveryDateAndPrice({
+              items: updatedCartItems,
+            })),
+          },
         })
-
-        // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-// Eklenen ürünün `clientId` değerini döndür
-        return updateCartItems.find(
-            (x)=>
-                x.product===item.product &&
-            x.color===item.color &&
-            x.size===item.size
-        )?.clientId!
-            },
-            init:()=>set({cart:initialState}),
-        }),
+        },
+        //sepeti başlangıç durumuna döndürme.sepeti sıfırlar
+  init: () => set({ cart: initialState }),
+    }),
+        
+        //zustand store'un persist yapılandırması
         {
             name:'cart-store', //Depolama alanında kullanılacak anahtar adı.
         }
